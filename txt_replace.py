968560f71ce834b5a49e6242f70ad792da517027
stupidegg8576 <stupidegg8576@gmail.com>
2022-05-04 10:17:01 +0800

    0504 back up

--- txt_replace.py
+++ txt_replace.py
@@ -1,6 +1,11 @@
 import os
 import re
 import yaml
+import regex
+
+
+def remove_control_characters(string):
+    return regex.sub(r'\p{C}', '', string)
 
 
 if __name__ == '__main__':
@@ -8,6 +13,8 @@ if __name__ == '__main__':
     # get files list
     logfloder_path = './TracelineLog/'
     log_paths = []
+
+    log_type = {}
 
     # load replace yaml
     replace_yaml_file = open('txt_replace.yaml', 'r', encoding='utf-8')
@@ -30,6 +37,8 @@ if __name__ == '__main__':
         log_lines = log_file.readlines()
 
         for i in range(len(log_lines)):
+            # remove control chars
+            log_lines[i] = remove_control_characters(log_lines[i])
 
             # find time in log
             # sometimes it will contain some noise at the front of log
@@ -39,13 +48,24 @@ if __name__ == '__main__':
             if time_search != None:
                 log_shift = time_search.span()[1]
                 log_lines[i] = log_lines[i][log_shift:]
+            raw_line = log_lines[i]
+            # replace model_name: model should be replace before keywords
+            for model in model_list:
+                # model names have a lot of variants,
+                # so consider #$%#$%(model_name)!@#$@#$ as a model_name
+                log_lines[i] = re.sub(
+                    r'(?i)' + model + '([0-9A-Za-z_-]{0,})', '_MODEL_NAME', log_lines[i])
+
+            # replace keywords
             for rule in replace_yaml:
                 log_lines[i] = re.sub(
                     replace_yaml[rule]['regex'], replace_yaml[rule]['replace'], log_lines[i])
-            for model in model_list:
-                log_lines[i] = re.sub(
-                    model, '_MODEL_NAME', log_lines[i])
+
+            if log_lines[i] not in log_type.keys():
+                log_type[log_lines[i]] = raw_line
         # write result to file
         log_file_replaced = open(log_path + '/syslog_replaced.log', 'w', encoding='utf-8',
                                  errors='ignore')
+        for i in range(len(log_lines)):
+            log_lines[i] = log_lines[i] + '\n'
         log_file_replaced.writelines(log_lines)
--- txt_to_logtype.py
+++ txt_to_logtype.py
@@ -15,7 +15,7 @@ class log():
 
 
 def remove_control_characters(string):
-    return regex.sub(r'\p{C}', '', string)
+    return regex.sub(r'[\p{C}\[\]\(\)]', '', string)
 
 
 def load_func_parse_data(path):
@@ -38,70 +38,27 @@ if __name__ == '__main__':
                                 '/' + report_ID + '/syslog_replaced.log')
 
     log_type = {}
-
+    max_len = 0
+    max_len_str = ''
     for log_path in logfiles:
 
         log_lines = open(log_path, 'r', encoding='utf-8',
                          errors='ignore').readlines()
-
         for line in log_lines:
-            """# line = Mar 24 07:49:49 wlceventd:
-            #        0            15 17
-            line = line.replace('\n', '')
-            shift = 0
-            while shift < len(line) - 15:
-                try:
-                    t = datetime.strptime(
-                        line[0 + shift:15 + shift], '%b %d %H:%M:%S').replace(year=log_year)
-                    break
-                except:
-                    shift = shift + 1"""
-            # remove \t \n or other control characters
             line = remove_control_characters(line)
+            splited_line = re.split('[: ,]+', line)
+            if len(splited_line) > max_len:
+                max_len = len(splited_line)
+                max_len_str = splited_line
+            temp = log_type
+            for i in range(len(splited_line)):
+                if splited_line[i] not in temp.keys():
+                    temp[splited_line[i]] = {}
+                temp = temp[splited_line[i]]
 
-            t = line.split()
-
-            while(len(t) < 3):
-                t.append('_')
-
-            for i in (0, 1, 2):
-                if t[i] == '':
-                    t[i] == '_'
-                if t[i][-1] == ':':
-                    t[i] = t[i][:-1]
-                t[i] = t[i].replace('[', '').replace(']', '')
-            # put splitted log in to dict to separate each type of logs
-            if t[0] not in log_type.keys():
-                log_type[t[0]] = {}
-            if t[1] not in log_type[t[0]].keys():
-                log_type[t[0]][t[1]] = {}
-            if t[2] not in log_type[t[0]][t[1]].keys():
-                log_type[t[0]][t[1]][t[2]] = {}
-                log_type[t[0]][t[1]][t[2]]['Count'] = 0
-                log_type[t[0]][t[1]][t[2]]['log'] = line
-
-            log_type[t[0]][t[1]][t[2]]['Count'] = \
-                log_type[t[0]][t[1]][t[2]]['Count'] + 1
-
-    # sort and count how many log in that type
-    for t0 in log_type:
-        for t1 in log_type[t0]:
-            count = 0
-            for t2 in log_type[t0][t1]:
-                count = count + log_type[t0][t1][t2]['Count']
-            # add line count after sorting
-            log_type[t0][t1] = dict([('Count', count)] +
-                                    sorted(log_type[t0][t1].items(), key=lambda x: x[1]['Count'], reverse=True))
-        count = 0
-        for t1 in log_type[t0]:
-            count = count + log_type[t0][t1]['Count']
-        log_type[t0] = dict([('Count', count)] +
-                            sorted(log_type[t0].items(), key=lambda x: x[1]['Count'], reverse=True))
-    count = 0
-    for t0 in log_type:
-        count = count + log_type[t0]['Count']
-    log_type = dict([('Total log lines', count)] +
-                    sorted(log_type.items(), key=lambda x: x[1]['Count'], reverse=True))
+    log_type = dict(sorted(log_type.items(), key=lambda x: x[0], reverse=True))
+    print(max_len)
+    print(max_len_str)
     # write result to file
     with open('logtype.yaml', 'w', encoding='utf-8') as file:
         yaml.dump(log_type, file, Dumper=yaml.SafeDumper,
