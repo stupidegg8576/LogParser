1465973d61b41ae981b17d6e7bf7d2fb8804197e
stupidegg8576 <stupidegg8576@gmail.com>
2022-05-24 10:50:05 +0800

    05/24 fix skip all

--- Traceline_driver_stat_parser/driver_stat_to_json.py
+++ Traceline_driver_stat_parser/driver_stat_to_json.py
@@ -62,22 +62,25 @@ def get_variable_name(line: str):
 
     # if there is no | in line
     elif '(' in line:
-        splited = regex.split(r'[ \t]', line)
+        line = regex.sub(r'[ \t]', '', line)
+        splited = regex.split(r'([\(\)])', line)
         variables = []
+        # find word in ( )
         for i in range(len(splited)):
-            if '(' in splited[i]:
-                # try to find if there is a word inside ( )
-                if regex.search(r'\(([A-Za-z0-9_\-]+)\)', splited[i]) != None:
-                    variables.append(regex.search(
-                        r'\(([A-Za-z0-9_\-]+)\)', splited[i]).group(1))
-                # if the string inside ( ) is not a word
-                # try the string before ( )
-                elif i != 0 and regex.search(r'[A-Za-z0-9_-]+', splited[i-1]):
-                    variables.append(regex.search(
-                        r'[A-Za-z0-9_-]+', splited[i-1]).group(0))
-                else:
-                    variables.append(
-                        'Something wrong in searching variable name')
+            if i + 2 < len(splited):
+                if splited[i] == '(' and splited[i+2] == ')':
+                    if regex.match(r'[A-Za-z0-9]', splited[i+1]) != None:
+                        variables.append(splited[i+1])
+        # if can't find a word, try the first split not empty
+        if len(variables) == 0:
+            for i in range(len(splited)):
+                if splited[i] != '':
+                    variables.append(splited[i+1])
+                    break
+        # if still can't find anything
+        if len(variables) == 0:
+            variables.append('Something wrong in searching variable name')
+
         return variables
 
     # return a empty string
@@ -120,6 +123,7 @@ if __name__ == '__main__':
     is_command = False
 
     result = {}
+    last_matched = -1
     for log_count in range(len(log_lines)):
 
         # start with ==============
@@ -149,42 +153,54 @@ if __name__ == '__main__':
                         # if already in it, goto next number
                         else:
                             continue
+                # skip command set that can't recognized
                 else:
+                    command_set = 'No matched'
                     pass
                     #raise BaseException("Found no matched command set")
 
             elif is_command:
-                format_start, format_end = find_command(
+                last_matched, format_end = find_command(
                     log_lines[log_count], format_lines)
-                format_count = format_start
-                if format_start != -1:
+                if last_matched != -1:
                     command_name = get_variable_name(
-                        format_lines[format_start])[0]
+                        format_lines[last_matched])[0]
                     if command_name not in result[command_set].keys():
                         result[command_set][command_name] = {}
+                # skip command that can't recognized
                 else:
+                    command_name = 'No matched'
                     pass
                     #raise BaseException("Found no matched command")
             # is normal log line
             # find a matched format line
             # and store value to result[command_set][command_name]
             else:
-
-                match = regex.match(
-                    format_lines[format_count], log_lines[log_count])
-                # if not matched, move to next format line
-                while match == None:
-                    format_count = format_count + 1
-                    if format_count > format_end:
-                        format_count = -1
-                        break
-                    match = regex.match(
-                        format_lines[format_count], log_lines[log_count])
-                # can't find match command
-                if format_count == -1:
+                # not a recognized command, or a wrong format
+                # skip
+                if last_matched == -1:
                     continue
 
-                variable_name = get_variable_name(format_lines[format_count])
+                match = regex.match(
+                    format_lines[last_matched], log_lines[log_count])
+                # if not matched, move to next format line
+                temp = last_matched
+                while match == None:
+                    temp = temp + 1
+                    if temp > format_end:
+                        temp = -1
+                        break
+                    match = regex.match(
+                        format_lines[temp], log_lines[log_count])
+                # can't find matched log format
+                # skip this log line
+                if match == None:
+                    log_count = log_count + 1
+                    continue
+                else:
+                    last_matched = temp
+
+                variable_name = get_variable_name(format_lines[last_matched])
                 # skip group(0)
                 for i in range(1, len(match)):
                     result[command_set][command_name][variable_name[i-1]
@@ -194,8 +210,8 @@ if __name__ == '__main__':
         print(command_set)
         for command in result[command_set]:
             print('\t' + command)
-            for func in result[command_set][command]:
-                print('\t\t' + func + ' : ' +
-                      result[command_set][command][func])
+            for var in result[command_set][command]:
+                print('\t\t' + var + ' : ' +
+                      result[command_set][command][var])
     with open(output_file_path, 'w', encoding='utf-8', errors='ignore') as output_file:
-        output_file.write(json.dumps(result, skipkeys=False))
+        output_file.write(json.dumps(result, skipkeys=True))
